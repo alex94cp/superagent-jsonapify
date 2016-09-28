edd3cd093370638a905368c1881b073942f335ec
kozo yamagata <tune002@gmail.com>
2016-09-28 19:07:22 +0900

    Fix detecting relationships data

---
+++ __tests__/common-test.js
@@ -0,0 +1,67 @@
+var common = require("../common");
+
+describe("parse", function() {
+  var sampleJSON = JSON.stringify({
+    data: {
+      type: "products",
+      id: "1",
+      attributes: {
+        "title": "Awesome product 1"
+      },
+      relationships: {
+        tags: {
+          data: [
+            {
+              id: "1",
+              type: "tags"
+            }
+          ]
+        },
+        comments: {
+          data: [
+            {
+              id: "1",
+              type: "comments"
+            }
+          ]
+        }
+      }
+    },
+    included: [
+      {
+        type: "tags",
+        id: "1",
+        attributes: {
+          name: "example tag 1"
+        }
+      },
+      {
+        type: "comments",
+        id: "1",
+        attributes: {
+          body: "example comment 1"
+        }
+      }
+    ]
+  });
+
+  it("should parse json-api format json", function() {
+    var result = common.parse(sampleJSON);
+    expect(result["data"]["tags"]).toEqual([{
+      type: "tags",
+      id: "1",
+      attributes: {
+        name: "example tag 1"
+      },
+      name: "example tag 1"
+    }]);
+    expect(result["data"]["comments"]).toEqual([{
+      type: "comments",
+      id: "1",
+      attributes: {
+        body: "example comment 1"
+      },
+      body: "example comment 1"
+    }]);
+  });
+});
